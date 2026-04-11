924aa85e5764054f1d30615dbfed4d3d4821bf14
Emerson <cardosoemerson501@gmail.com>
2026-04-11 23:34:07 +0200

    feat:conexao com base de dado na cloud e configuracao do .env

--- backend/backend/src/test/java/com/libry/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/libry/backend/BackendApplicationTests.java
@@ -1,9 +1,8 @@
 package com.libry.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
+
 class BackendApplicationTests {
 
 	@Test
